8c0235c962f03a88d7e83cc2439191863b3ca39b
Fred Zerpa <fred.zerpa@elangel.edu.ve>
2022-05-02 15:53:22 -0400

    Add comments

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
+// VARIABLES
 const PORT = process.env.PORT || 8000;
 const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@clusterangel.ifu9f.mongodb.net/angel?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
+// MONGO CONNECTION
 mongoose.connect(MONGO_URI);
 mongoose.connection.on('open', () => console.log('MongoDB connection ready!'));
 mongoose.connection.on('error', console.error);
