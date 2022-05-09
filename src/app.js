c9b3fabcb5e4ad9b3758d1ef750ccad2516f533d
Fred Zerpa <fred.zerpa@elangel.edu.ve>
2022-05-09 15:41:20 -0400

    Parse data coming from Forms

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const studentsRouter = require('./routes/students/students.router');
 
 const app = express();
 
-// Parse incoming requests with JSON payloads 
+// Parse incoming requests with JSON payloads
 app.use(express.json());
+
+// Parse incoming requests with Form payloads
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 app.use('/students', studentsRouter);
 
