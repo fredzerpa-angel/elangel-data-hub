542ca0c48f66b42c1e234ba430f5f16376ec0d2b
Fred Zerpa <fred.zerpa@elangel.edu.ve>
2022-05-17 13:41:32 -0400

    Add Parents Model & Mongo Schema

---
+++ src/models/parents/parents.model.js
@@ -0,0 +1,39 @@
+const parents = require('./parents.mongo');
+
+async function getAllParents() {
+  return await parents.find({});
+}
+
+async function createParent(parentData) {
+  return await parents.create(parentData);
+}
+
+async function updateParent(parentId, updateData) {
+  const options = {
+    new: true, // Retorna el Padre con los datos actualizados
+    runValidators: true, // Aplica las validaciones del Parent Schema otra vez
+  };
+
+  return await parents.findByIdAndUpdate(parentId, updateData, options);
+}
+
+async function deleteParent(parentId) {
+  return await parents.findByIdAndDelete(parentId);
+}
+
+async function getParentById(parentId) {
+  return await parents.findById(parentId);
+}
+
+async function getStudentBySearch({ searchBy, value }) {
+  return await students.find({ [searchBy]: new RegExp(value, 'gi') });
+}
+
+module.exports = {
+  getAllParents,
+  createParent,
+  updateParent,
+  deleteParent,
+  getParentById,
+  getStudentBySearch,
+};
---
+++ src/models/parents/parents.mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const parentSchema = new mongoose.Schema({
+  // Data proveniente de Arcadat
+  cedulaId: {
+    type: String,
+    required: false,
+  },
+  fullname: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  phoneMain: {
+    type: String,
+    required: false,
+  },
+  phoneSecondary: {
+    type: String,
+    required: false,
+  },
+});
+
+// Conecta studentSchema con "Students" colleccion
+module.exports = mongoose.model('Parent', parentSchema);
