57027a0ee68ce27d7e7a4b182171046adc17396f
Fred Zerpa <fredzerpa@gmail.com>
2023-03-06 06:01:30 -0400

    Add DB Restore API

--- server/src/api/MongoDB/MongoDB.api.js
+++ server/src/api/MongoDB/MongoDB.api.js
@@ -6,7 +6,7 @@ const DUMP_INITIAL_OPTIONS = {
   filePrefix: null, // Si solo se usa el prefix se agregara un sufijo de la fecha actual en formato ISO
   fileFullname: null, // Reemplaza el nombre completo del archivo a crear
   folderPathToDump: null, // Si es nulo se creara el backup en el mismo directorio
-  config: path.join(__dirname, 'config.yaml'),
+  config: path.join(__dirname, 'config.yaml'), // ref: https://www.mongodb.com/docs/database-tools/mongorestore/#std-option-mongorestore.--archive
 }
 
 // Estos son 'dumps' son temporales, ya que el backup se guarda en AWS S3
@@ -33,6 +33,34 @@ function dumpDatabase(dumpOptions = DUMP_INITIAL_OPTIONS) {
 
 }
 
+const RESTORE_INITIAL_OPTIONS = {
+  fileName: null, // Nombre completo (excluyendo la extension) del archivo. Ex: 'el-angel-2023-01-01'
+  filePath: null, // Si es nulo buscara en la ruta del directorio actual
+  // ! Confirmar las autorizaciones del usuario para insertar documentos en la BD
+  config: path.join(__dirname, 'config.yaml'), // ref: https://www.mongodb.com/docs/database-tools/mongorestore/#std-option-mongorestore.--archive
+}
+
+async function restoreDatabase(restoreOptions = RESTORE_INITIAL_OPTIONS) {
+  const options = { ...RESTORE_INITIAL_OPTIONS, ...restoreOptions };
+
+  const RESTORE_FILE_NAME = `${options.fileName}.archive.gz`;
+  const RESTORE_FILE_PATH = path.join(options.filePath ?? '', RESTORE_FILE_NAME);
+  const cmd = `mongorestore --config="${options.config}" --archive="${RESTORE_FILE_PATH}" --gzip`;
+
+  try {
+    execSync(cmd);
+    return {
+      ok: true,
+    }
+  } catch (err) {
+    return {
+      ok: false,
+      error: err
+    }
+  }
+}
+
 module.exports = {
   dumpDatabase,
+  restoreDatabase,
 }
