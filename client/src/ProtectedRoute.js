c014690fd66e37c75eee3cf45fc7eaeba11d679a
Fred Zerpa <fredzerpa@gmail.com>
2023-03-08 05:34:24 -0400

    Upload client template

---
+++ client/src/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useAuth } from './auth-context/auth.context';
+import { useNavigate, Outlet } from 'react-router-dom';
+
+export const ProtectedRoute = () => {
+  const navigate = useNavigate();
+  let { user } = useAuth();
+
+  useEffect(() => {
+    if (!user || !user.token || user.token === "") {
+      navigate("/authentication/sign-in")
+    }
+  }, [navigate, user])
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
---
+++ client/src/index.js
@@ -0,0 +1,36 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v4.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+import { AuthProvider } from "./auth-context/auth.context";
+
+// Soft UI Dashboard React Context Provider
+import { SoftUIControllerProvider } from "context";
+
+let user = localStorage.getItem("user");
+user = JSON.parse(user);
+
+createRoot(document.getElementById("root"))
+  .render(
+    <BrowserRouter>
+      <SoftUIControllerProvider>
+        <AuthProvider userData={user}>
+          <App />
+        </AuthProvider>
+      </SoftUIControllerProvider>
+    </BrowserRouter>
+  );
