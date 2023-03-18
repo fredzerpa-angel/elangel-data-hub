9f02c9aecf185fc0fbcd04089411262440959331
Fred Zerpa <fredzerpa@gmail.com>
2023-03-18 12:08:22 -0400

    Remove unused variables on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 // react-router components
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
