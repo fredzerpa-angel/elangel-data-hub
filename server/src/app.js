7715a85fa85656eff988d6f56773a44bd770ba09
Fred Zerpa <fredzerpa@gmail.com>
2023-09-02 05:55:55 -0400

    Add Compression feature

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
-const expressStaticGzip = require("express-static-gzip");
+const fallback = require('express-history-api-fallback'); // 
+const compression = require('compression'); // Comprime los archivos al mandarlos al cliente
 require('dotenv').config();
 
 // Routes
@@ -20,7 +21,6 @@ if (process.env.NODE_ENV === 'development') {
     origin: 'http://localhost:3000',
   }))
 }
-
 // Transforma requests entrantes con datos tipo JSON
 app.use(express.json({ limit: '50mb' })); // Cambiamos el limite para poder obtener decenas de miles de datos JSON, como los pagos
 // Transforma requests entrantes con datos tipo Form
@@ -30,13 +30,15 @@ app.use(fileUpload()); // Middleware usado para recibir los archivos enviados po
 // Creamos una sesion para el usuario logueado y guardamos los datos en una Cookie en el cliente
 app.use(cookieParser(process.env.COOKIE_SESSION_SECRET))
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(compression()); // Envia los archivos al cliente comprimidos
 
 // Se centraran todas las rutas del servidor (Estudiantes, Pagos, ...) en el ruta '/api'
 app.use('/api', checkUserAuth, apiRouter);
 app.use('/auth', authRouter); // LogIn, LogOut y Registro de usuario
 
 // Enviamos el Front End a cada endpoint
-app.get('/*', expressStaticGzip(path.join(__dirname, '..', 'public')));
+const root = path.join(__dirname, '..', 'public'); // Path de la carpeta del Build hecho por React
+app.use(express.static(root));
+app.use(fallback('index.html', { root }));
 
 module.exports = app;
